56c3c3f6d6b741f45d2014e949a46c5543758b96
tsukigahara <satsuki.tksm@gmail.com>
2022-11-14 22:06:02 +0100

    added js first FOR loop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-var myList;
-
-for (let i = 1; i < 6) {
-    myList.window.prompt("Crea la tua lista con 5 oggetti\nOggetto no."+i);
+var myList = [];
+for (let i = 0; i < 5; i++) {
+    myList[i] = window.prompt("Crea la tua lista con 5 oggetti\nOggetto no."+(i + 1));
     console.log(myList);
 }
+//node chiedere 
+var divHTML = document.getElementById("main");
+var listHTML = document.createElement("ol");
+listHTML.classList.add("list-group");
+listHTML.classList.add("list-group-numbered");
+divHTML.append(listHTML);
+for (let i = 0; i < 5; i++) {
+    var listItem = document.createElement("li")
+    listItem.classList.add("list-group-item")
+    listHTML.appendChild (listItem);
+    listItem.innerHTML = myList[i];
+}
+
+// document.createElement("ul");
+// listHTML.classList.add("list-group list-group-numbered");
+// document.body.appendChild(listHTML);
+// let index = 0;
+// while (index < 5) {
+//     var listItem = document.createElement("li")
+//     listItem.classList.add("list-group-item");
+//     listHTML.appendChild (listItem);
+//     listItem.innerHTML = myList[index];
+//     index++;
+// }
