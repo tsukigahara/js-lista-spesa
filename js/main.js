240e684f750f988c740e66b6d7e078ae36c5bb9d
tsukigahara <satsuki.tksm@gmail.com>
2022-11-15 10:13:03 +0100

    master 1.0

--- js/main.js
+++ js/main.js
@@ -5,25 +5,29 @@ for (let i = 0; i < 5; i++) {
 }
 //node chiedere 
 var divHTML = document.getElementById("main");
+
 var listHTML = document.createElement("ol");
 listHTML.classList.add("list-group");
 listHTML.classList.add("list-group-numbered");
+listHTML.classList.add("my-5");
 divHTML.append(listHTML);
 for (let i = 0; i < 5; i++) {
-    var listItem = document.createElement("li")
-    listItem.classList.add("list-group-item")
+    var listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
     listHTML.appendChild (listItem);
     listItem.innerHTML = myList[i];
 }
 
-// document.createElement("ul");
-// listHTML.classList.add("list-group list-group-numbered");
-// document.body.appendChild(listHTML);
-// let index = 0;
-// while (index < 5) {
-//     var listItem = document.createElement("li")
-//     listItem.classList.add("list-group-item");
-//     listHTML.appendChild (listItem);
-//     listItem.innerHTML = myList[index];
-//     index++;
-// }
+var listHTML = document.createElement("ol");
+listHTML.classList.add("list-group");
+listHTML.classList.add("list-group-numbered");
+listHTML.classList.add("my-5");
+divHTML.append(listHTML);
+let index = 0;
+while (index < 5) {
+    var listItem = document.createElement("li");
+    listItem.classList.add("list-group-item");
+    listHTML.appendChild (listItem);
+    listItem.innerHTML = myList[index];
+    index++;
+}
